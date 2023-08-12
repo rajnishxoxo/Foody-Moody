2ae7870e7d1bf543c70aa0d970129fb104884512
Rajnish <srajnish235.rs@gmail.com>
2023-08-12 20:46:51 +0530

    Created Header

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Header from './src/Comp/Header.js'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const App = () => {
   return (
     <div>
-      
+      <Header/>
     </div>
   );
 };
