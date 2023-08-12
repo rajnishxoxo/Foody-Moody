24eb54793f3e708fefdd753ba5b491bb7ec56f72
Rajnish <srajnish235.rs@gmail.com>
2023-08-12 20:27:49 +0530

    Created App
    created root

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+root.render(<App/>)
