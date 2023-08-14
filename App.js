6841a6cda764ee610b4825193ad46a7388b3321a
Rajnish <srajnish235.rs@gmail.com>
2023-08-14 16:02:49 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from './src/Comp/Header.js'
 import Body from "./src/Comp/Body.js";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const App = () => {
@@ -14,4 +15,12 @@ const App = () => {
   );
 };
 
-root.render(<App/>)
+const appRoute =createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>
+  }
+])
+
+
+root.render(<RouterProvider router={appRoute}/>)
