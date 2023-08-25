16bf7ba9dff5ca48909e4b14d31f65d74ff3c6d1
Rajnish <srajnish235.rs@gmail.com>
2023-08-25 11:43:28 +0530

    Created User.js

--- src/Routes/About.js
+++ src/Routes/About.js
@@ -1,9 +1,28 @@
-const About =()=>{
-    return(
-        <div>
-            <h1>This is About Us Section</h1>
-        </div>
-    )
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div className="about-us-Section">
+        <p className="intro-Para">
+          Welcome to Swiggy, your go-to destination for delicious food delivered
+          right to your doorstep. At Swiggy, we're on a mission to revolutionize
+          the way you experience food delivery. Our platform brings together a
+          curated selection of restaurants, a seamless ordering process, and
+          top-notch delivery services to create an unparalleled dining
+          experience.
+        </p>
+        <h3 className="heading-Mission"> Our Mission</h3>
+        <p className="intro-Para">
+          Our mission is simple: to connect food enthusiasts with the flavors
+          they love, all while supporting local businesses. We're committed to
+          delivering not just food but also convenience, choice, and moments of
+          delight.
+        </p>
+        <h2 className="heading-Mission">Meet the Team :</h2>
+      </div>
+    );
+  }
 }
 
 export default About;
