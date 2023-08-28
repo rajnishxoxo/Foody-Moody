e370f9c8464335142c67540cca91347e5dfd0427
Rajnish <srajnish235.rs@gmail.com>
2023-08-28 22:28:32 +0530

    Update Contact.js

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -14,14 +14,14 @@ Let's take a step ahead and help you better.</h1>
                 <li className="text-base  text-white font-medium px-20 py-25  cursor-pointer flex relative mt-2 mb-2 ">Swiggy FAQs</li>
 
             </ul>
-            <div className="w-1/2 bg-gray-400  ">
-                <h1>Swiggy One FAQs</h1>
-                <ul className=" p-3 flex flex-col justify-evenly">
-                    <li>Is there a limit on the number of devices I can use Swiggy One on?</li>
-                    <li>Are Swiggy One benefits available at all restaurants?</li>
-                    <li>Is there a minimum order value to avail Swiggy One Flat ₹150 Off coupons on Dineout?</li>
-                    <li>Is there a limit on free deliveries or extra discounts?</li>
-                    <li>Is extra discount applicable on item total?</li>
+            <div className="w-full bg-gray-400  ">
+                <h1 className="text-xl mx-80">Swiggy One FAQs</h1>
+                <ul className=" p-3 flex flex-col justify-stretch">
+                    <li className=" text-base  text-white font-medium px-20 py-25  cursor-pointer flex relative mt-3 mb-3">Is there a limit on the number of devices I can use Swiggy One on?</li>
+                    <li className=" text-base  text-white font-medium px-20 py-25  cursor-pointer flex relative  mt-3 mb-3 ">Are Swiggy One benefits available at all restaurants?</li>
+                    <li className=" text-base  text-white font-medium px-20 py-25  cursor-pointer flex relative  mt-3 mb-3 ">Is there a minimum order value to avail Swiggy One Flat ₹150 Off coupons on Dineout?</li>
+                    <li className=" text-base  text-white font-medium px-20 py-25  cursor-pointer flex relative  mt-3 mb-3 ">Is there a limit on free deliveries or extra discounts?</li>
+                    <li className=" text-base  text-white font-medium px-20 py-25  cursor-pointer flex relative  mt-3 mb-3 ">Is extra discount applicable on item total?</li>
                 </ul>
             </div>
         </div>
