7502d7d6adac7e588681455c01578b4091009c5d
Rajnish <srajnish235.rs@gmail.com>
2023-08-28 22:16:59 +0530

    Update Contact.js

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -5,8 +5,8 @@ const Contact =()=>{
 Let's take a step ahead and help you better.</h1>
         </div>
 
-        <div>
-            <ul className="p-5 top-20 bg-gray-500 w-72 h-80 flex flex-col justify-evenly">
+        <div className="flex flex-row w-full">
+            <ul className="p-5 top-20 bg-gray-500 w-3/2 h-80 flex flex-col justify-evenly  mr-4">
                 <li className=" text-base  text-white font-medium px-20 py-25  cursor-pointer flex relative ">Help with orders</li>
                 <li className=" text-base  text-white font-medium px-20 py-25  cursor-pointer flex relative mt-2 mb-2 ">General issues</li>
                 <li className=" text-base  text-white font-medium px-20 py-25  cursor-pointer flex relative mt-2 mb-2 ">Partner Onboarding</li>
@@ -14,6 +14,16 @@ Let's take a step ahead and help you better.</h1>
                 <li className="text-base  text-white font-medium px-20 py-25  cursor-pointer flex relative mt-2 mb-2 ">Swiggy FAQs</li>
 
             </ul>
+            <div className="w-1/2 bg-gray-400  ">
+                <h1>Swiggy One FAQs</h1>
+                <ul className=" p-3 flex flex-col justify-evenly">
+                    <li>Is there a limit on the number of devices I can use Swiggy One on?</li>
+                    <li>Are Swiggy One benefits available at all restaurants?</li>
+                    <li>Is there a minimum order value to avail Swiggy One Flat ₹150 Off coupons on Dineout?</li>
+                    <li>Is there a limit on free deliveries or extra discounts?</li>
+                    <li>Is extra discount applicable on item total?</li>
+                </ul>
+            </div>
         </div>
     </>
         
