83bbedf312857de763beb0e5d09c54fa6676aa0a
Rajnish <srajnish235.rs@gmail.com>
2023-08-30 20:53:24 +0530

    Update RestroMenuCategory.js

--- src/Comp/RestroMenuCategory.js
+++ src/Comp/RestroMenuCategory.js
@@ -1,9 +1,27 @@
 const RestroMenuCategory = (data) => {
   const { title, itemCards } = data.data;
 
-  console.log(title, ">", itemCards);
+  const listItem = itemCards;
+  //   console.log(listItem)
 
-  return <div></div>;
+  return (
+    <div className="w-1/2 bg-gray-50 text-center  shadow-lg p-4 mx-auto my-4">
+      <div className="flex flex-row justify-between">
+        <span>
+          {title} -({itemCards.length})
+        </span>
+        <span>⬇️</span>
+      </div>
+
+      {
+        <div>
+          {listItem.map((data) => {
+            console.log(data);
+          })}
+        </div>
+      }
+    </div>
+  );
 };
 
 export default RestroMenuCategory;
