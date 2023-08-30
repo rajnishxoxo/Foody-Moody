266b7d3249193e90cdbfaca4b0bf1f0bc796a77a
Rajnish <srajnish235.rs@gmail.com>
2023-08-30 23:01:17 +0530

    Update RestroMenuCategory.js

--- src/Comp/RestroMenuCategory.js
+++ src/Comp/RestroMenuCategory.js
@@ -4,6 +4,8 @@ const RestroMenuCategory = (data) => {
   const listItem = itemCards;
   //   console.log(listItem)
 
+  
+
   return (
     <div className="w-1/2 bg-gray-50 text-center  shadow-lg p-4 mx-auto my-4">
       <div className="flex flex-row justify-between">
@@ -15,9 +17,29 @@ const RestroMenuCategory = (data) => {
 
       {
         <div>
+          <div>
           {listItem.map((data) => {
-            console.log(data);
+            // console.log(data)
+            const {name,description,price,imageId} = data.card.info;
+            console.log(name,description,price,imageId)
+            return(
+                <div key={data.card.info.id} className="p-2 m-2 border-b-2">
+                    <div className="flex">
+                    <h3 className=" mr-3 text-1.22 font-medium text-gray-700 break-words">{name}</h3>
+                    <div>
+                    <span>Rs {price/100}</span>
+                    </div>
+                    </div>
+                    <div className="mt-4  mb-3 leading-5 text-opacity-45 tracking-tighter text-base text-gray-500 flex">
+                    {description ? description : "New to try"}
+                    </div>
+                    <div>
+                        <img className="w-28 h-24 object-cover object-center rounded-6" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/"+imageId}/>
+                    </div>
+                </div>
+            )
           })}
+          </div>
         </div>
       }
     </div>
