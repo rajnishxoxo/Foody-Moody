e285cb48e6d293f97c547b387ffddc70db4ba4d2
Rajnish <srajnish235.rs@gmail.com>
2023-09-14 14:28:22 +0530

    Update RestroMenuCategory.js

--- src/Comp/RestroMenuCategory.js
+++ src/Comp/RestroMenuCategory.js
@@ -40,72 +40,62 @@ const RestroMenuCategory = ({ data }) => {
       </div>
 
       {showItem && (
-        <div>
-          <div>
-            {listItem.map((data) => {
-              const { name, description, price, imageId } = data.card.info;
+  <div>
+    <div>
+      {listItem.map((data) => {
+        const { name, description, price, imageId } = data.card.info;
 
-              // console.log(name,description,price,imageId)
-              return (
-                <div
-                  className="flex flex-row justify-evenly items-center 	"
-                  key={data.card.info.id}
-                >
-                  <div
-                    key={data.card.info.id}
-                    className="p-2 m-2 border-b-2 flex  justify-evenly w-full"
-                  >
-                    <div>
-                      <h3 className=" mr-3 text-1.22 text-left font-medium text-gray-700 break-words">
-                        {name}
-                      </h3>
-                      <div>
-                        {price ? (
-                          <p className=" text-left m-2 font-medium text-gray-700 break-words ">
-                            Rs {price / 100}
-                          </p>
-                        ) : (
-                          "Rs 200"
-                        )}
-                      </div>
-                      <div className="mt-4  text-left mb-3 leading-5 text-opacity-45 tracking-tighter text-base text-gray-500 flex">
-                        {description ? description : "New to try"}
-                      </div>
-                    </div>
-                  </div>
+        return (
+          <div
+            className="flex flex-row justify-between items-center border-b-2 p-2 m-2"
+            key={data.card.info.id}
+          >
+            <div className="flex flex-col">
+              <h3 className="text-2xl text-gray-700 font-medium break-words">
+                {name}
+              </h3>
+              <p className="text-lg text-gray-700 mt-2">
+                {price ? `Rs ${price / 100}` : "Rs 200"}
+              </p>
+              <div className="text-base text-gray-500 mt-2">
+                {description ? description : "New to try"}
+              </div>
+            </div>
 
-                  <div className="flex">
-                    {imageId ? (
-                      <img
-                        className="w-28 h-24 object-cover object-center rounded-6"
-                        src={
-                          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/" +
-                          imageId
-                        }
-                      />
-                    ) : (
-                      <img
-                        className="w-28 h-24 object-cover object-center rounded-6"
-                        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/z2wc6ywih0dtidtwhaoq"
-                      />
-                    )}
-                    <div className="absolute ">
-                      <button
-                        className="w-14 left-1/2 bottom-8 transform -translate-x-3/2  -translate-y-7/2 z-10  p-2 bg-gray-100 shadow-lg  text-green-700 h-full cursor-pointer rounded "
-                        onClick={() => {
-                          handleAddItem(data);
-                        }}
-                      >
-                        Add+
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
+            <div className="relative">
+              {imageId ? (
+                <img
+                  className="w-32 h-32 object-cover object-center rounded-6"
+                  src={
+                    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/" +
+                    imageId
+                  }
+                  alt={name}
+                />
+              ) : (
+                <img
+                  className="w-32 h-32 object-cover object-center rounded-6"
+                  src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/z2wc6ywih0dtidtwhaoq"
+                  alt="Default"
+                />
+              )}
+              <button
+                className="absolute inset-0 w-full h-full bg-gray-100 opacity-0 hover:opacity-100 flex justify-center items-center cursor-pointer rounded transition-opacity duration-300"
+                onClick={() => {
+                  handleAddItem(data);
+                }}
+              >
+                Add+
+              </button>
+            </div>
           </div>
-        </div>
-      )}
+        );
+      })}
+    </div>
+  </div>
+)}
+
+
     </div>
   );
 };
